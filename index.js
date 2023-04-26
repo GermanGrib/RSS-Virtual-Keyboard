c621dc3654f3d66e4934c32dc7ed0c0881edb06c
German Gribanov <germangribanov@mail.ru>
2023-04-26 16:03:11 +0300

    feat: backspace, delete, enter functionallity

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fullKeyBoardKeys = [
   {9: {'en': 'Tab', 'ru': 'Таб'}, 81: {'en': 'q', 'ru': 'й'}, 87: {'en': 'w', 'ru': 'ц'}, 69: {'en': 'e', 'ru': 'у'}, 82: {'en': 'r', 'ru': 'к'}, 84:{'en': 't', 'ru': 'е'}, 89: {'en': 'y', 'ru': 'н'}, 85: {'en': 'u', 'ru': 'г'}, 73:{'en': 'i', 'ru': 'ш'}, 79: {'en': 'o', 'ru': 'щ'}, 80: {'en': 'p', 'ru': 'з'}, 219:{'en': '[','ru': 'х'}, 221:{'en': ']','ru': 'ъ'}, 220:{'en': '\\','ru': '\\','shiften': '|', 'shiftru': '/'}, 46:{'en': 'Delete','ru': 'Дэлит'}},
   {20:{'en': 'CapsLk', 'ru': 'КапсЛк'}, 65: {'en': 'a', 'ru': 'ф'}, 83: {'en': 's', 'ru': 'ы'}, 68: {'en': 'd', 'ru': 'в'}, 70:{'en': 'f', 'ru': 'а'}, 71: {'en': 'g', 'ru': 'п'}, 72: {'en':'h', 'ru': 'р'}, 74: {'en': 'j', 'ru': 'о'}, 75: {'en': 'k', 'ru': 'л'}, 76: {'en': 'l', 'ru': 'д'}, 186: {'en': ';', 'ru': 'ж','shiften': ':', 'shiftru': 'Ж'}, 222:{'en': "'",'ru': 'э','shiften': '"', 'shiftru': 'Э'}, 13: {'en': 'Enter', 'ru': 'Энтер'}},
   {16:{'en': 'Shift', 'ru': 'Шифт'},220: {'en': '\\','ru': '\\'}, 88:{'en': 'z', 'ru': 'я'}, 90: {'en': 'x', 'ru': 'ч'}, 67: {'en': 'c', 'ru': 'с'}, 86: {'en': 'v', 'ru': 'м'}, 66: {'en': 'b', 'ru': 'и'}, 78:{'en': 'n', 'ru': 'т'}, 77: {'en': 'm','ru': 'ь'}, 188: {'en': ',', 'ru': 'б','shiften': '<', 'shiftru': 'Ю'}, 190: {'en': '.', 'ru': 'ю','shiften': '>', 'shiftru': 'Ю'}, 191:{'en': '/', 'ru': '.','shiften': '?', 'shiftru': ','}, 38:{'en': '&uarr;', 'ru': '&uarr;'}, 16: {'en': 'Shift', 'ru': 'Шифт'}},
-  {17: {'en':'Ctrl', 'ru': 'Ктрл'}, 91: {'en': 'Win', 'ru': 'Вин'}, 18:{'en': 'Alt', 'ru': 'Алт'}, 32:{'en': ' ', 'ru': ' '}, 18: {'en': 'Alt','ru': 'Алт'}, 17: {'en': 'Ctrl', 'ru': 'Ктрл'}, 37: {'en': '&larr;','ru': '&larr;'}, 40:{'en': '	&darr;', 'ru': '	&darr;'}, 39: {'en':'&rarr;', 'ru': '&rarr;'}}
+  {17: {'en':'Ctrl', 'ru': 'Ктрл'}, 91: {'en': 'Win', 'ru': 'Вин'}, 18:{'en': 'Alt', 'ru': 'Алт'}, 32:{'en': ' ', 'ru': ' '}, 18: {'en': 'Alt','ru': 'Алт'}, 17: {'en': 'Ctrl', 'ru': 'Ктрл'}, 37: {'en': '&larr;','ru': '&larr;'}, 40:{'en': '&darr;', 'ru': '&darr;'}, 39: {'en':'&rarr;', 'ru': '&rarr;'}}
 ];
 const keyboardKeysCode = [
   [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8],
@@ -14,26 +14,28 @@ const keyboardKeysCode = [
 ];
 const userEvents = ['keydown', 'keyup', 'mousedown', 'mouseup',];
 
-let currentLanguage = 'en';
+let currentLanguage = localStorage.getItem('currLang') || 'en';
 let flag = false;
 
 function initFirstScreen() {
-  const body = document.body;
-  const keyboardSection = document.createElement('section');
-  const keyboardContainer = document.createElement('div');
-  const textScreen = document.createElement('textarea');
-  const textScreenPlaceholder = 'To change language use Ctrl + Alt . . . \nKeyboard was created on Windows';
+  const BODY = document.body;
+  const KEYBOARD_SECTION = document.createElement('section');
+  const KEYBOARD_CONTAINER = document.createElement('div');
+  const TEXT_AREA = document.createElement('textarea');
+  const TEXT_SCREEN_PLACE_HOLDER = (currentLanguage === 'en') ?
+    'Change language use: Ctrl + Alt . . . \nKeyboard was created on Windows':
+    'Сменить язык клацай Ктрл + Алт . . . \nКлава была сделана на Винде';
 
-  keyboardSection.classList = 'keyboard';
-  body.prepend(keyboardSection);
+  KEYBOARD_SECTION.classList = 'keyboard';
+  BODY.prepend(KEYBOARD_SECTION);
 
-  keyboardContainer.classList = 'keyboard__container';
-  keyboardSection.prepend(keyboardContainer);
+  KEYBOARD_CONTAINER.classList = 'keyboard__container';
+  KEYBOARD_SECTION.prepend(KEYBOARD_CONTAINER);
 
-  textScreen.classList = 'keyboard__screen';
-  textScreen.placeholder = textScreenPlaceholder;
-  keyboardSection.prepend(textScreen);
-  initKeyboardsBtns(keyboardContainer);
+  TEXT_AREA.classList = 'keyboard__screen';
+  TEXT_AREA.placeholder = TEXT_SCREEN_PLACE_HOLDER;
+  KEYBOARD_SECTION.prepend(TEXT_AREA);
+  initKeyboardsBtns(KEYBOARD_CONTAINER);
 }
 
 function initKeyboardsBtns(keyboardContainer) {
@@ -67,15 +69,19 @@ function initKeyboardsBtns(keyboardContainer) {
     }
   });
 
-  let backSlashKey = document.querySelectorAll('[data-key="220"]');
-  backSlashKey[0].dataset.location = 0;
-  backSlashKey[1].dataset.location = 0;
-  let winKey = document.querySelector('[data-key="91"]');
-  winKey.dataset.location = 1;
+  const BACK_SLAH_KEYS = document.querySelectorAll('[data-key="220"]');
+  BACK_SLAH_KEYS[0].dataset.location = 0;
+  BACK_SLAH_KEYS[1].dataset.location = 0;
+  const WINDOWS_KEY = document.querySelector('[data-key="91"]');
+  WINDOWS_KEY.dataset.location = 1;
 }
-
+//! Finish it  usage of ES6+ features (classes, property destructuring, etc): 
+//! Need yo handle this the Shift, Alt, Ctrl, Caps lock and Space keys should work as on a real keyboard
 //! Need to handle & <> - don't work
 function handleUserEvents(event) {
+  const ENTER = 13;
+  const DELETE = 46;
+  const TAB = 9;
   const KEY_BTNS = Array.from(document.querySelectorAll('.keyboard__key'));
   const TEXT_AREA = document.querySelector('.keyboard__screen');
   const CAPSLOCK = 20;
@@ -83,7 +89,6 @@ function handleUserEvents(event) {
   const ALT = 18;
   const BACKSPACE = 8;
   const SHIFT = 16;
-  const ARROW_KEYS = [37, 38, 39, 40];
   const SYMBOL_KEYS = ['55', '188', '190'];
   let currentKeyBtn;
   let isMouseDownOrKeyDown = event.type === 'mousedown' || event.type === 'keydown';
@@ -92,6 +97,9 @@ function handleUserEvents(event) {
   currentKeyBtn = (event.type === 'mousedown' || event.type === 'mouseup') ? event.target :
     currentKeyBtn = KEY_BTNS.find((btn) =>(Number(btn.dataset.key) === event.keyCode && Number(btn.dataset.location) === event.location)); 
 
+  if (event.type === 'keyup' || event.type === 'keydown') {
+    event.preventDefault();
+  }
   TEXT_AREA.focus();
 
   let currentDatasetKey = Number(currentKeyBtn.dataset.key);
@@ -107,20 +115,37 @@ function handleUserEvents(event) {
     changeKeyboardLanguage(event);
   }
 
-  if((currentDatasetKey === BACKSPACE && event.type === 'keydown') || (currentDatasetKey === BACKSPACE && event.type === 'mousedown')) { //Backspace
+  if((currentDatasetKey === BACKSPACE && event.type === 'keydown') || (currentDatasetKey === BACKSPACE && event.type === 'mousedown')) {
     TEXT_AREA.value = TEXT_AREA.value.slice(0, -1);
   }
 
-  if ((ARROW_KEYS.includes(currentDatasetKey) && event.type === 'mouseup') ||
-   (ARROW_KEYS.includes(currentDatasetKey) && event.type === 'keyup')) { 
-    moveCursor(currentDatasetKey);
+  if (currentDatasetKey === CAPSLOCK && isMouseUpOrKeyUp) { 
+    toggleCase(event,currentDatasetKey);
+  } else if (currentDatasetKey === SHIFT) { 
+    toggleCase(event,currentDatasetKey);
   }
 
-  if (currentDatasetKey === CAPSLOCK && event.type === 'keyup') { 
-    toggleCase(event);
-  } else if (currentDatasetKey === SHIFT) { 
-    toggleCase(event);
-  } 
+  if (currentDatasetKey === TAB && isMouseUpOrKeyUp) {
+    TEXT_AREA.value = TEXT_AREA.value + '  ';
+  }
+
+  if (currentDatasetKey === DELETE && isMouseUpOrKeyUp) {
+    const index = TEXT_AREA.selectionStart;
+    const newValue = TEXT_AREA.value.substring(0, index) + TEXT_AREA.value.substring(index + 1);
+    TEXT_AREA.value = newValue;
+    TEXT_AREA.selectionStart = index;
+    TEXT_AREA.selectionEnd = index;
+  }
+
+  if (currentDatasetKey === ENTER && isMouseUpOrKeyUp) {
+    const newLine = '\n'; 
+    const cursorPos = TEXT_AREA.selectionStart; 
+    const textBeforeCursor = TEXT_AREA.value.substring(0, cursorPos); 
+    const textAfterCursor = TEXT_AREA.value.substring(cursorPos); 
+    TEXT_AREA.value = textBeforeCursor + newLine + textAfterCursor; 
+    TEXT_AREA.selectionStart = cursorPos + 1; 
+    TEXT_AREA.selectionEnd = cursorPos + 1; 
+  }
 
   if(currentKeyBtn.classList.contains('keyboard__key') && isMouseDownOrKeyDown) {
     currentKeyBtn.classList.add('is-active');
@@ -161,59 +186,61 @@ function changeKeyboardLanguage(event){
     });
   
     TEXT_SCREEN.placeholder = (currentLanguage === 'en') ?
-      'To change language use Ctrl + Alt . . . \nKeyboard was created on Windows' :
-      'Сменить язык клацай Ктрл + Алт . . . \nклава была сделана на Винде';
+      'Change language use: Ctrl + Alt . . . \nKeyboard was created on Windows' :
+      'Сменить язык клацай Ктрл + Алт . . . \nКлава была сделана на Винде';
   }
+
+  localStorage.setItem('currLang', currentLanguage);
 }
 
-function toggleCase(event) {
+function toggleCase(event, currentDatasetKey) {
   const SHIFT = 16;
   const CAPSLOCK = 20;
-  const keyBtns = Array.from(document.querySelectorAll('.keyboard__key'));
+  const CAPSLK_BTN = document.querySelector('[data-key="20"]');
+  const KEY_BTNS = Array.from(document.querySelectorAll('.keyboard__key'));
   let firstLett = document.querySelector('[data-key="81"]').textContent;
   let isLettLowerCase = (firstLett === firstLett.toLowerCase()) ? true : false;
-  let capsLk = document.querySelector('[data-key="20"]');
 
-  if ((event.keyCode === CAPSLOCK && !capsLk.classList.contains('caps')) ||
-   (event.keyCode === SHIFT && event.type === 'mousedown' || event.keyCode === SHIFT && event.type === 'keydown')){ 
+  if ((currentDatasetKey === CAPSLOCK && !CAPSLK_BTN.classList.contains('caps')) ||
+  (currentDatasetKey === SHIFT && event.type === 'mousedown' || currentDatasetKey === SHIFT && event.type === 'keydown')){ 
     if (isLettLowerCase) {
-      keyBtns.forEach(key => {
+      KEY_BTNS.forEach(key => {
         const text = key.textContent;
         if (text.length === 1) {
           key.textContent = text.toUpperCase();
         }
       });
-      if(event.keyCode === CAPSLOCK){
-        capsLk.classList.add('caps');
+      if(currentDatasetKey === CAPSLOCK){
+        CAPSLK_BTN.classList.add('caps');
       }
     } 
   } else { 
     
-    keyBtns.forEach(key => {
+    KEY_BTNS.forEach(key => {
       const text = key.textContent;
       if (text.length === 1) {
         key.textContent = text.toLowerCase();
       }
     });
-    if(event.keyCode === CAPSLOCK && capsLk.classList.contains('caps')){
-      capsLk.classList.remove('caps');
+    if(currentDatasetKey === CAPSLOCK && CAPSLK_BTN.classList.contains('caps')){
+      CAPSLK_BTN.classList.remove('caps');
     }
   }
 
 
-  if (event.keyCode === SHIFT && event.type === 'mousedown' || event.keyCode === SHIFT && event.type === 'keydown') {
+  if (currentDatasetKey === SHIFT && event.type === 'mousedown' || currentDatasetKey === SHIFT && event.type === 'keydown') {
       
-    for(let i = 0; i < keyBtns.length; i++) {
-      const key = keyBtns[i];
+    for(let i = 0; i < KEY_BTNS.length; i++) {
+      const key = KEY_BTNS[i];
       const dataKey = key.getAttribute('data-key');
       const obj = fullKeyBoardKeys.find(item => item.hasOwnProperty(dataKey));
       if (obj && `shift${currentLanguage}` in obj[dataKey]) {
         key.innerHTML = obj[dataKey][`shift${currentLanguage}`]
       }
     }
-  } else if(event.keyCode === SHIFT && event.type === 'mouseup' || event.keyCode === SHIFT && event.type === 'keyup') {
-    for(let i = 0; i < keyBtns.length; i++) {
-      const key = keyBtns[i];
+  } else if(currentDatasetKey === SHIFT && event.type === 'mouseup' || currentDatasetKey === SHIFT && event.type === 'keyup') {
+    for(let i = 0; i < KEY_BTNS.length; i++) {
+      const key = KEY_BTNS[i];
       const dataKey = key.getAttribute('data-key');
       const obj = fullKeyBoardKeys.find(item => item.hasOwnProperty(dataKey));
       if (obj && currentLanguage in obj[dataKey]) {
@@ -222,57 +249,6 @@ function toggleCase(event) {
     }
   }
 
-}
-
-function moveCursor(key) {
-  const textArea = document.querySelector('.keyboard__screen');
-
-  switch (String(key)) {
-  case '38':
-    moveCursorUp();
-    break;
-  case '40':
-    moveCursorDown();
-    break;
-  case '37':
-    moveCursorLeft();
-    break; 
-  case '39':
-    moveCursorRight();
-    break;
-  }
-
-  function moveCursorUp() {
-    const currentPosition = textArea.selectionStart;
-    const newPosition = currentPosition - textArea.cols;
-    if (newPosition >= 0) {
-      textArea.setSelectionRange(newPosition, newPosition);
-    }
-  }
-
-  function moveCursorDown() {
-    const currentPosition = textArea.selectionStart;
-    const newPosition = currentPosition + textArea.cols;
-    if (newPosition <= textArea.value.length) {
-      textArea.setSelectionRange(newPosition, newPosition);
-    }
-  }
-  
-  function moveCursorLeft() {
-    const currentPosition = textArea.selectionStart;
-    const newPosition = currentPosition - 1;
-    if (newPosition >= 0) {
-      textArea.setSelectionRange(newPosition, newPosition);
-    }
-  }
-  
-  function moveCursorRight() {
-    const currentPosition = textArea.selectionStart;
-    const newPosition = currentPosition + 1;
-    if (newPosition <= textArea.value.length) {
-      textArea.setSelectionRange(newPosition, newPosition);
-    }
-  }
 }
 
 initFirstScreen();
