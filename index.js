2818b9942f57e141f2e8948e97a614b47e8520ca
German Gribanov <germangribanov@mail.ru>
2023-04-30 15:09:44 +0300

    fix: backspace delete letter from select, add eslint-disable-next-line

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const fullKeyBoardKeys = [
   {192: {'en': '`', 'ru': 'ё', 'shiften': '~', 'shiftru': 'Ё'}, 49: {'en': '1', 'ru': '1', 'shiften': '!', 'shiftru': '!'}, 50: {'en': '2', 'ru': '2','shiften': '@', 'shiftru': '"'}, 51: {'en': '3', 'ru': '3','shiften': '#', 'shiftru': '№'}, 52: {'en': '4', 'ru': '4','shiften': '$', 'shiftru': ';'}, 53:{'en': '5', 'ru': '5','shiften': '%', 'shiftru': '%'}, 54: {'en': '6', 'ru': '6','shiften': '^', 'shiftru': ':'}, 55: {'en': '7', 'ru': '7','shiften': '&', 'shiftru': '?'}, 56: {'en': '8', 'ru': '8','shiften': '*', 'shiftru': '*'}, 57: {'en': '9', 'ru': '9','shiften': '(', 'shiftru': '('}, 48:{'en': '0', 'ru': '0','shiften': ')', 'shiftru': ')'}, 189:{'en': '-', 'ru': '-','shiften': '_', 'shiftru': '_'}, 187: {'en': '=', 'ru': '=','shiften': '+', 'shiftru': '+'}, 8:{'en': 'Backspace', 'ru': 'Бэкспейс'}},
   {9: {'en': 'Tab', 'ru': 'Таб'}, 81: {'en': 'q', 'ru': 'й'}, 87: {'en': 'w', 'ru': 'ц'}, 69: {'en': 'e', 'ru': 'у'}, 82: {'en': 'r', 'ru': 'к'}, 84:{'en': 't', 'ru': 'е'}, 89: {'en': 'y', 'ru': 'н'}, 85: {'en': 'u', 'ru': 'г'}, 73:{'en': 'i', 'ru': 'ш'}, 79: {'en': 'o', 'ru': 'щ'}, 80: {'en': 'p', 'ru': 'з'}, 219:{'en': '[','ru': 'х'}, 221:{'en': ']','ru': 'ъ'}, 220:{'en': '\\','ru': '\\','shiften': '|', 'shiftru': '/'}, 46:{'en': 'Delete','ru': 'Дэлит'}},
+  // eslint-disable-next-line
   {20:{'en': 'CapsLk', 'ru': 'КапсЛк'}, 65: {'en': 'a', 'ru': 'ф'}, 83: {'en': 's', 'ru': 'ы'}, 68: {'en': 'd', 'ru': 'в'}, 70:{'en': 'f', 'ru': 'а'}, 71: {'en': 'g', 'ru': 'п'}, 72: {'en':'h', 'ru': 'р'}, 74: {'en': 'j', 'ru': 'о'}, 75: {'en': 'k', 'ru': 'л'}, 76: {'en': 'l', 'ru': 'д'}, 186: {'en': ';', 'ru': 'ж','shiften': ':', 'shiftru': 'Ж'}, 222:{'en': "'",'ru': 'э','shiften': '"', 'shiftru': 'Э'}, 13: {'en': 'Enter', 'ru': 'Энтер'}},
+  // eslint-disable-next-line
   {16:{'en': 'Shift', 'ru': 'Шифт'},220: {'en': '\\','ru': '\\'}, 88:{'en': 'z', 'ru': 'я'}, 90: {'en': 'x', 'ru': 'ч'}, 67: {'en': 'c', 'ru': 'с'}, 86: {'en': 'v', 'ru': 'м'}, 66: {'en': 'b', 'ru': 'и'}, 78:{'en': 'n', 'ru': 'т'}, 77: {'en': 'm','ru': 'ь'}, 188: {'en': ',', 'ru': 'б','shiften': '<', 'shiftru': 'Ю'}, 190: {'en': '.', 'ru': 'ю','shiften': '>', 'shiftru': 'Ю'}, 191:{'en': '/', 'ru': '.','shiften': '?', 'shiftru': ','}, 38:{'en': '&uarr;', 'ru': '&uarr;'}, 16: {'en': 'Shift', 'ru': 'Шифт'}},
+  // eslint-disable-next-line
   {17: {'en':'Ctrl', 'ru': 'Ктрл'}, 91: {'en': 'Win', 'ru': 'Вин'}, 18:{'en': 'Alt', 'ru': 'Алт'}, 32:{'en': ' ', 'ru': ' '}, 18: {'en': 'Alt','ru': 'Алт'}, 17: {'en': 'Ctrl', 'ru': 'Ктрл'}, 37: {'en': '&larr;','ru': '&larr;'}, 40:{'en': '&darr;', 'ru': '&darr;'}, 39: {'en':'&rarr;', 'ru': '&rarr;'}}
 ];
 const keyboardKeysCode = [
@@ -50,26 +53,22 @@ function initKeyboardsBtns(keyboardContainer) {
       keyboardKey.classList.add('keyboard__key');
       keyboardKey.innerHTML = fullKeyBoardKeys[i][currentKey][currentLanguage];
       keyboardKey.dataset.key = currentKey;
+      keyboardKey.dataset.location = 0;
       keyboardRow.appendChild(keyboardKey);
     }
 
     keyboardContainer.appendChild(keyboardRow);
   }
 
-  const keyboardKeys = document.querySelectorAll('.keyboard__key');
-
-  keyboardKeys.forEach((key, index) => {
-    key.dataset.location = 0;
-    key.dataset.key = key.dataset.key || index; 
-  
-    const keysWithSameDataKey = document.querySelectorAll(`[data-key="${key.dataset.key}"]`);
-    if (keysWithSameDataKey.length > 1) {
-      keysWithSameDataKey.forEach((key, index) => {
-        key.dataset.location = index + 1;
-      });
-    }
-  });
-
+  const SHIFTS = document.querySelectorAll('[data-key="16"]');
+  SHIFTS[0].dataset.location = 1;
+  SHIFTS[1].dataset.location = 2;
+  const CTRLS = document.querySelectorAll('[data-key="17"]');
+  CTRLS[0].dataset.location = 1;
+  CTRLS[1].dataset.location = 2;
+  const ALTS = document.querySelectorAll('[data-key="18"]');
+  ALTS[0].dataset.location = 1;
+  ALTS[1].dataset.location = 2;
   const BACK_SLAH_KEYS = document.querySelectorAll('[data-key="220"]');
   BACK_SLAH_KEYS[0].dataset.location = 0;
   BACK_SLAH_KEYS[1].dataset.location = 0;
@@ -94,7 +93,7 @@ function handleUserEvents(event) {
   let isMouseUpOrKeyUp = event.type === 'mouseup' || event.type === 'keyup';
 
   currentKeyBtn = (event.type === 'mousedown' || event.type === 'mouseup') ? event.target :
-    currentKeyBtn = KEY_BTNS.find((btn) =>(Number(btn.dataset.key) === event.keyCode && Number(btn.dataset.location) === event.location)); 
+    KEY_BTNS.find((btn) =>(Number(btn.dataset.key) === event.keyCode && Number(btn.dataset.location) === event.location)); 
 
   if (event.type === 'keyup' || event.type === 'keydown') {
     event.preventDefault();
@@ -115,7 +114,11 @@ function handleUserEvents(event) {
   }
 
   if((currentDatasetKey === BACKSPACE && event.type === 'keydown') || (currentDatasetKey === BACKSPACE && event.type === 'mousedown')) {
-    TEXT_AREA.value = TEXT_AREA.value.slice(0, -1);
+    const cursorPos = TEXT_AREA.selectionStart;
+    let newValue = TEXT_AREA.value.substring(0, cursorPos - 1) + TEXT_AREA.value.substring(cursorPos);
+    TEXT_AREA.value = newValue;
+    TEXT_AREA.selectionStart = cursorPos - 1;
+    TEXT_AREA.selectionEnd = cursorPos - 1;   
   }
 
   if (currentDatasetKey === CAPSLOCK && isMouseUpOrKeyUp) { 
@@ -178,6 +181,7 @@ function changeKeyboardLanguage(event, currentDatasetKey){
       const {key: currentKey} = key.dataset;
   
       fullKeyBoardKeys.forEach(keysRow => {
+        // eslint-disable-next-line
         if (keysRow.hasOwnProperty(currentKey)) {
           key.innerHTML = keysRow[currentKey][currentLanguage];
         }
@@ -232,20 +236,24 @@ function toggleCase(event, currentDatasetKey) {
     for(let i = 0; i < KEY_BTNS.length; i++) {
       const key = KEY_BTNS[i];
       const dataKey = key.getAttribute('data-key');
+      // eslint-disable-next-line
       const obj = fullKeyBoardKeys.find(item => item.hasOwnProperty(dataKey));
       if (obj && `shift${currentLanguage}` in obj[dataKey]) {
         key.innerHTML = obj[dataKey][`shift${currentLanguage}`];
       }
+      CAPSLK_BTN.classList.remove('caps');
     }
   } else if(currentDatasetKey === SHIFT && event.type === 'mouseup' || currentDatasetKey === SHIFT && event.type === 'keyup') {
     for(let i = 0; i < KEY_BTNS.length; i++) {
       const key = KEY_BTNS[i];
       const dataKey = key.getAttribute('data-key');
+      // eslint-disable-next-line
       const obj = fullKeyBoardKeys.find(item => item.hasOwnProperty(dataKey));
       if (obj && currentLanguage in obj[dataKey]) {
         key.innerHTML = obj[dataKey][currentLanguage];
       }
     }
+    CAPSLK_BTN.classList.remove('caps');
   }
 
 }
